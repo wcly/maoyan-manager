30f4c9a6df808d6cf64c9a268a8c16190f1fff29
wcly <git@github.com:wcly>
2020-05-02 08:08:56 +0800

    feat: 加入平面对象装换处理

--- server/src/entities/Movie.ts
+++ server/src/entities/Movie.ts
@@ -1,33 +1,46 @@
-import { IsNotEmpty, ArrayMinSize, IsInt, Min, Max } from 'class-validator';
+import { IsNotEmpty, ArrayMinSize, IsInt, Min, Max, IsArray } from 'class-validator';
+import { Type } from 'class-transformer';
+import 'reflect-metadata'
 
 export class Movie {
     @IsNotEmpty({ message: '电影名称不可以为空' })
+    @Type(() => String)
     public name: string;
 
     @IsNotEmpty({ message: '电影类型不能为空' })
     @ArrayMinSize(1, { message: '电影类型至少有一个' })
+    @IsArray({message: '电影类型必须是一个数组'})
+    @Type(() => String)
     public types: string[];
 
     @IsNotEmpty({ message: '上映地区不能为空' })
     @ArrayMinSize(1, { message: '上映地区至少有一个' })
+    @IsArray({message: '上映地区必须是一个数组'})
+    @Type(() => String)
     public areas: string[];
 
     @IsNotEmpty({ message: '时长不可以为空' })
     @IsInt({ message: '时长必须是整数' })
     @Min(1, { message: '时长最小是一分钟' })
     @Max(99999, { message: '时长过长' })
+    @Type(() => Number)
     public timeLong: number
 
     @IsNotEmpty({ message: "是否热映不可以为空" })
+    @Type(() => Boolean)
     public isHot: boolean = false
 
     @IsNotEmpty({ message: "是否即将上映不可以为空" })
+    @Type(() => Boolean)
     public isComing: boolean = false
 
     @IsNotEmpty({ message: "是否是经典影片不可以为空" })
+    @Type(() => Boolean)
     public isClasic: boolean = false
 
+    @Type(() => String)
     public description?: string
 
+    @Type(() => String)
     public poster?: string
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import { Movie } from "./entities/Movie";
 import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer'
 
-const m = new Movie()
+const n: any = {}
+n.name = 'sss';
+const m = plainToClass(Movie, n as object)
+
 validate(m).then(errors => {
     console.log(errors)
 })
