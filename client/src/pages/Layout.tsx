dfc9b36ec0b27ea0b1d81f2658a8d1d513b7b90d
wcly <git@github.com:wcly>
2020-05-10 10:58:54 +0800

    feat: 加入layout布局

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -4,26 +4,41 @@ import Home from './Home'
 import AddMovie from './movie/AddMovie'
 import EditMovie from './movie/EditMovie'
 import MovieList from './movie/MovieList'
+import { Layout, Menu } from 'antd'
 
-const Layout: React.FC = function () {
-    return <div>
-        <header>
-            <ul>
-                <li>
-                    <NavLink to="/">首页</NavLink>
-                </li>
-                <li><NavLink to="/movie">电影</NavLink></li>
-                <li><NavLink to="/movie/add">添加电影</NavLink></li>
-                <li><NavLink to="/movie/edit/235r487">修改电影</NavLink></li>
-            </ul>
-            <div>
-                <Route path="/" component={Home} exact/>
-                <Route path="/movie" component={MovieList} exact/>
-                <Route path="/movie/add" component={AddMovie}/>
-                <Route path="/movie/edit/:id" component={EditMovie}/>
-            </div>
-        </header>
-    </div>
+const { Header, Sider, Content } = Layout
+
+const _Layout: React.FC = function () {
+    return (
+        <div className="container">
+            <Layout>
+                <Header className="header"><NavLink to="/">猫眼电影管理系统</NavLink></Header>
+                <Layout>
+                    <Sider>
+                        <Menu
+                            mode="inline"
+                            theme="dark"
+                        >
+                            <Menu.Item key="1">
+                               <NavLink to="/movie">电影列表</NavLink>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <NavLink to="/movie/add">添加电影</NavLink>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Content>
+                        <div className="main">
+                            <Route path="/" component={Home} exact />
+                            <Route path="/movie" component={MovieList} exact />
+                            <Route path="/movie/add" component={AddMovie} />
+                            <Route path="/movie/edit/:id" component={EditMovie} />
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
+        </div>
+    )
 }
 
-export default Layout
+export default _Layout
