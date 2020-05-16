7644511d3e61ccdb19dbd76d3fe59fcf6f45e01c
wcly <git@github.com:wcly>
2020-05-16 08:19:58 +0800

    feat: 修改服务器index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import Express from 'express'
 import MovieRouter from './routes/MovieRoutes'
 import UploadRouter from './routes/UploadRoute'
+import history from 'connect-history-api-fallback'
 
 const app = Express();
 
+app.use(history()) // 防止在其它路由刷新页面找不到路径的情况，会自动找根目录下的index.html文件
 app.use("/upload", Express.static("public/upload"))
+app.use("/", Express.static("public/build"))
 
 app.use(Express.json()); // 配置中间件，将请求体转化为json格式
 
